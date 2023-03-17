35554c5bac4fe35e2f7f4edc4d915f3a67f7594b
pathan02 <127297102+pathan02@users.noreply.github.com>
2023-03-17 19:28:02 +0530

    Update production.rb
    as during confirmation link we are getting a ink with including the port number, so i have made changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   #config for the devise production
 
-  config.action_mailer.default_url_options = { host: 'chalo.digital',port: 25060}
+  config.action_mailer.default_url_options = { host: 'chalo.digital'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
