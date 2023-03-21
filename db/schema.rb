638776c3249f9b8ce20b429d0271b1e25cf1501b
pathan02 <127297102+pathan02@users.noreply.github.com>
2023-03-21 12:44:14 +0530

    changed db colname  (#69)
    * changed db
    
    * changed test cases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_162519) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_070012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_162519) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "fname"
-    t.string "lname"
+    t.string "sname"
     t.string "phno"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
