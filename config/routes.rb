905c11eb09a698e11f7fb0e43db3a3081e30b773
pathan02 <shoaib@blueinsight.digital>
2023-03-24 16:38:29 +0530

    complete-management-of-dealers-by-admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  get 'dealerships/show'
+  
+  root "home#index"
   devise_for :users
   resources :metadata
   resources :variants
   resources :cars
+  #routes for dealership managements
+  get 'dealerships/show'
+  get 'dealerships/verified'
+  get  'dealerships/toggle'
+  get  'dealerships/toggle1'
+  get 'dealerships/manage_dealers'
+
+  
 
   
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
-
+  
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
- end
- 
+  end
+  
 end
